2cf1e9eee4ae864c26ddf392f33ac87dce5bd9b0
jafar99 <jafaralihajari@gmail.com>
2021-07-28 23:40:55 +0530

    intial commit

---
+++ assets/js/config.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCLGNuV0fC8A0UBy3Gxw97kEcmd7scNM0c",
+  authDomain: "aasara-5b685.firebaseapp.com",
+  databaseURL: "https://aasara-5b685-default-rtdb.firebaseio.com",
+  projectId: "aasara-5b685",
+  storageBucket: "aasara-5b685.appspot.com",
+  messagingSenderId: "486112847585",
+  appId: "1:486112847585:web:111f209b83ea38a0ddf9a0",
+  measurementId: "G-LELX76V252"
+};
+  
+  firebase.initializeApp(firebaseConfig);
---
+++ assets/js/script.js
@@ -0,0 +1,113 @@
+$( document ).ready(function() {
+    var w = window.innerWidth;
+
+    if(w > 767){
+        $('#menu-jk').scrollToFixed();
+    }else{
+       // $('#menu-jk').scrollToFixed();
+    }
+
+
+
+})
+
+
+$( document ).ready(function() {
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        autoplay: true,
+        dots: true,
+        autoplayTimeout: 5000,
+        navText:['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
+
+});
+
+
+
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
+
+
+
+
+
+// Init slick slider + animation
+$('.slider').slick({
+  autoplay: true,
+  speed: 800,
+  lazyLoad: 'progressive',
+  arrows: true,
+  dots: false,
+	prevArrow: '<div class="slick-nav prev-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
+	nextArrow: '<div class="slick-nav next-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
+}).slickAnimation();
+
+
+
+$('.slick-nav').on('click touch', function(e) {
+
+    e.preventDefault();
+
+    var arrow = $(this);
+
+    if(!arrow.hasClass('animate')) {
+        arrow.addClass('animate');
+        setTimeout(() => {
+            arrow.removeClass('animate');
+        }, 1600);
+    }
+
+});
+
+
+
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
+
+
+
+
